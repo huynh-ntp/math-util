28191fca54461d47f5d89d58bae047d06d8f0636
huynh-ntp <phihuynh120120@gmail.com>
2020-11-14 12:12:43 +0700

    2nd: import static modification

--- src/math/util/MathUtil.java
+++ src/math/util/MathUtil.java
@@ -1,7 +1,9 @@
 
 package math.util;
 
-import huynhntp.util.MathUtility;
+import static huynhntp.util.MathUtility.*;
+
+// Khai bao san ham static goi ham static khong can cham nua
 
 
 public class MathUtil {
@@ -10,21 +12,22 @@ public class MathUtil {
     public static void main(String[] args) {
         
         long expected = 120;
-        long actual = MathUtility.getFactorial(5);
+        long actual = getFactorial(5);
         System.out.println("Expected: " + expected + " actial: "+ actual);
         
         
         // Expected 6! = 720
-        System.out.println("6! = " + MathUtility.getFactorial(6));
+        System.out.println("6! = " +getFactorial(6));
         
         // Expected : 1 if tinh 0!
-        System.out.println("0! = " + MathUtility.getFactorial(0));
+        System.out.println("0! = " + getFactorial(0));
         
         // Expected : IlligalArgumentException if tinh -5!
+       
+//        System.out.println("-5!: = " + MathUtility.getFactorial(-5));
         
-        System.out.println("-5!: = " + MathUtility.getFactorial(-5));
-        
-        
+        // in PI coi co giong Math.PI hem
+        System.out.println("PI: " +PI);
     }
     
 }
