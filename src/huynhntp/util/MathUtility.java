f63597fb8c561d977f8e792db7ec1bdbd720555b
huynh-ntp <phihuynh120120@gmail.com>
2020-11-18 08:00:17 +0700

    optimize the getFactorial() by using recursion

--- src/huynhntp/util/MathUtility.java
+++ src/huynhntp/util/MathUtility.java
@@ -13,28 +13,45 @@ public class MathUtility {
     // class fake class Math cua JDK
     // Math cua JDK co .abs() .sqrt()
     // chung toan la static, vi la thu vien dung chung
-    
+
     public static final double PI = 3.1415;
-    
+
     // ham tinh giai thua : n! = 1*2*3*4*5
     // 0! = ! = 1
     // khong tinh duoc giai thua am
     // 21! cuc lon , tran kieu long , khong chua noi
     // int toi da hai ty may
     // ham giai thua viet trong gioi han dau vao tu 0 den 20
-    
-    public static long getFactorial(int n){
-        // Nem ngoai le
-        if(n<0 || n>20) throw new IllegalArgumentException("n must be between 0 .. 20");
-        if(n==0 || n==1) return 1;
-        // khong sai else, co else tru diem
-        long result = 1;
-        // n = 2 -> 20
-        for (int i = 2; i <= n; i++) {
-            result*=i;
+//    public static long getFactorial(int n){
+//        // Nem ngoai le
+//        if(n<0 || n>20) throw new IllegalArgumentException("n must be between 0 .. 20");
+//        if(n==0 || n==1) return 1;
+//        // khong sai else, co else tru diem
+//        long result = 1;
+//        // n = 2 -> 20
+//        for (int i = 2; i <= n; i++) {
+//            result*=i;
+//        }
+//        
+//        return result;      
+//    }
+    // viết đệ quy cho hoành tráng
+    // gọi chính mình
+    public static long getFactorial(int n) {
+
+        if (n < 0 || n > 20) {
+            throw new IllegalArgumentException("n must be between 0...20");
         }
-        
-        return result;      
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * getFactorial(n - 1);
+
     }
-    
+
+    // Minh họa khái niệm Refactoring - tối ưu, chỉnh sửa lại code
+    // cho tốt hơn
+    // mình họa khái niệm REGRESSION TEST , test hồi quy, test lại
+    // 
+    // 
 }
